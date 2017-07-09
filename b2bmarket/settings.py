7db07f776a315c8cd898ffb4ff9e60c45ca9a5a7
Kenneth Vo <kennyvo91@gmail.com>
2017-07-08 23:20:16 -0500

    bootstrap and navbar working

--- b2bmarket/settings.py
+++ b2bmarket/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main_app',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
+"""App views are here"""
+
+
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
@@ -9,11 +12,15 @@ from .models import Listing
 
 
 def index(request):
+    """Show all objects"""
+
     listings = Listing.objects.all()
     form = ListingForm()
     return render(request, 'index.html', {'listings': listings, 'form':form})
 
 def register(request):
+    """Register new users"""
+
     if request.method == 'POST':
         form = SignUpForm(request.POST or None)
         if form.is_valid():
@@ -28,42 +35,56 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 
 def detail(request, listing_id):
+    """"Get detail of one listing"""
+
     listing = Listing.objects.get(id=listing_id)
     return render(request, 'detail.html', {'listing': listing})
 
 def update_listing(request, pk, template_name='request_form.html'):
+    """Update a listing"""
+
     listing = get_object_or_404(Listing, pk=pk)
-    form = ListingForm(request.POST or None, instance = listing)
+    form = ListingForm(request.POST or None, instance=listing)
     if form.is_valid():
         form.save()
         return redirect('/')
     return render(request, template_name, {'form': form})
 
 def delete_listing(request, pk, template_name='listing_confirm_delete.html'):
+    """Delete a listing"""
+
     listing = get_object_or_404(Listing, pk=pk)
-    if request.method =='POST':
+    if request.method == 'POST':
         listing.delete()
         return redirect('/')
     return render(request, template_name, {'object': listing})
 
 def post_listing(request, template_name='request_form.html'):
+    """Create a new Listing"""
+
     form = ListingForm(request.POST)
     if form.is_valid():
-        listing = form.save(commit = False)
+        listing = form.save(commit=False)
         listing.user = request.user
         listing.save()
         return redirect('/')
     return render(request, template_name, {'form': form})
 
 def profile(request, username):
+    """Go to a user's profile"""
+
     user = User.objects.get(username=username)
     listings = Listing.objects.filter(user=user)
     return render(request, 'profile.html', {'username': username, 'listings': listings})
 
 def request_form(request):
+    """Go to the request form"""
+
     return render(request, 'request_form.html')
 
 def login_view(request):
+    """Go to login"""
+
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -83,5 +104,7 @@ def login_view(request):
         return render(request, 'login.html', {'form': form})
 
 def logout_view(request):
+    """Logout"""
+
     logout(request)
     return HttpResponseRedirect('/')
